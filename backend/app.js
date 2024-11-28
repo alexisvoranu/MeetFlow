9ef2cdf11360c0fdb7aff67f924a2dbdd6776542
Alex Isvoranu <alx.isvoranu@yahoo.com>
2024-11-28 16:45:26 +0200

    Inițializare proiect Express

---
+++ backend/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json()); 
+app.use(express.urlencoded({ extended: true })); 
+app.use(morgan(process.env.LOG_LEVEL || 'dev')); 
+
+import indexRoutes from './routes/script.js';
+app.use('/', indexRoutes);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Serverul rulează la adresa http://localhost:${PORT}`);
+});
