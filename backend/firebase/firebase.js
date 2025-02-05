68d64c0984093efaa9f8a1fd8f7f15d80d2664ac
Alex Isvoranu <alx.isvoranu@yahoo.com>
2025-02-05 13:35:15 +0200

    Imbunatatire validari login

--- backend/firebase/firebase.js
+++ backend/firebase/firebase.js
@@ -1,17 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqOsojSBqNM7LjULCe9wrOQCGAYZMkv4Y",
-  authDomain: "meetflow-e5950.firebaseapp.com",
-  databaseURL:
-    "https://meetflow-e5950-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "meetflow-e5950",
-  storageBucket: "meetflow-e5950.firebasestorage.app",
-  messagingSenderId: "788639562123",
-  appId: "1:788639562123:web:745b0c9de417d53bd0978e",
-  measurementId: "G-9D3JQVR23B",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
